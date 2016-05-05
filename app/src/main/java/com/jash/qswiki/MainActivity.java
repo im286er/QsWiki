d0372619c854b9416af970990cda85b212f0f4e8
jash <hdmj@sina.com>
2016-05-05 15:33:35 +0800

    提交测试

--- app/src/main/java/com/jash/qswiki/MainActivity.java
+++ app/src/main/java/com/jash/qswiki/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //提交测试
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
